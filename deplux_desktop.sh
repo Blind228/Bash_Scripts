da8ca22e497af4995a74323640c4aee91cce1afe
blind228 <blind228@gmail.com>
2021-12-13 23:06:42 -0500

    Cleaned the output of script

--- deplux_desktop.sh
+++ deplux_desktop.sh
@@ -4,42 +4,6 @@
 # This is a basic install, easily configurable to your needs.
 
 
-#############################################################################################
-#######################################   FUNCTIONS   #######################################
-#############################################################################################
-
-# Optional package install
-optional_install() {
-    
-read -r -p "Do you want to install $optionalpkg? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-    echo " 
-    
-        Installing $optionalpkg
-
-    "
-    sudo apt install $optionalpkg
-    echo "
-______________________________________________________________________________________________________    
-                           $optionalpkg has been installed
-______________________________________________________________________________________________________
-"
-
-else 
-    echo "$optionalpkg won't be installed"
- 
-fi
-}
-
-
-#############################################################################################
-#######################################   VARIBALES   #######################################
-#############################################################################################
-
-# List of optional packages
-optionalpkgs=("discord" "") # TODO
-
 
 #############################################################################################
 #######################################    SCRIPT     #######################################
@@ -52,51 +16,63 @@ then
  exit 1
 fi
 
+
+####################################### LOG FILE #######################################
+# add log file (moslty for practice in scripting) TODO
+
 ####################################### SYSTEM CHECKS #######################################
 
 # Ensure system is up to date
-sudo apt update -y
-echo "Updating Linux..."
+echo "
+Updating Linux..."
+sudo apt-get update -qq
+echo "Done updating Linux
+"
 
 # Upgrade the system
-sudo apt upgrade -y
 echo "Upgrading Linux..."
+sudo apt-get upgrade -qq > /dev/null
+echo "Done upgrading Linux
+"
 
-####################################### SECURITY / BASIC COMMS #######################################
+#################################### OS CUSTOMISATIONS #######################################
+
+# .bashrc customisation TODO
+
+
+# Message of the day TODO
+
+
+# Rename home folder to remove capitalized letters TODO
+
+
+################################### SECURITY / BASIC COMMS ###################################
 
 # Install OpenSSH
 echo "Installing OpenSSH..."
-sudo apt install openssh-server -y
+sudo apt-get install openssh-server -qq > /dev/null
+echo "Done installing OpenSSH"
+# Disabling root login over ssh
+echo "PermitRootLogin no" >> /etc/ssh/sshd_config
+echo "PermitEmptyPasswords no" >> /etc/ssh/sshd_config
+echo "Root login by ssh disabled 
+"
 
-# SFTP Server / FTP server that runs over ssh TODO check settings
-echo "Setting up SFTP..."
-echo "
-Match group sftp
-ChrootDirectory /home
-X11Forwarding no
-AllowTcpForwarding no
-ForceCommand internal-sftp
-" >> /etc/ssh/sshd_config
-
-sudo service ssh restart  
-
-# configure the firewall TODO add ftp?
+# configure the firewall
 echo "Setting up ufw..."
-sudo ufw allow OpenSSH
+sudo ufw allow OpenSSH > /dev/null
 
 # Enable Firewall
 echo "Enabling ufw..."
-sudo ufw enable 
-
-# Disabling root login 
-echo "PermitRootLogin no" >> /etc/ssh/sshd_config 
-echo "PermitEmptyPasswords no" >> /etc/ssh/sshd_config
+sudo ufw enable > /dev/null
+echo "Done setting up and enabling ufw
+"
 
 # Fail2Ban install
 echo "Installing Fail2Ban..."
-sudo apt install -y fail2ban
-sudo systemctl start fail2ban
-sudo systemctl enable fail2ban
+sudo apt-get install fail2ban -qq > /dev/null
+sudo systemctl start fail2ban > /dev/null
+sudo systemctl enable fail2ban &> /dev/null
 
 echo "
 [sshd]
@@ -106,76 +82,78 @@ filter = sshd
 logpath = /var/log/auth.log
 maxretry = 4
 " >> /etc/fail2ban/jail.local
+echo "Done installing and setting up Fail2Ban
+"
 
+# more packages? TODO
 
 ####################################### TOOLS #######################################
 
 # SpeedTest Install
 echo "Installing speedtest-cli..."
-sudo apt install speedtest-cli -y
+sudo apt-get install speedtest-cli -qq > /dev/null
+echo "Done installing Speedtest
+"
 
 # Exa Install
-wget -q -P /tmp/ http://archive.ubuntu.com/ubuntu/pool/universe/r/rust-exa/exa_0.9.0-4_amd64.deb
 echo "Installing Exa..."
-sudo apt install /tmp/exa_0.9.0-4_amd64.deb
-rm /tmp/exa_0.9.0-4_amd64.deb
+wget -q -P ~/downloads   http://archive.ubuntu.com/ubuntu/pool/universe/r/rust-exa/exa_0.9.0-4_amd64.deb
+sudo apt-get install ~/downloads/exa_0.9.0-4_amd64.deb -qq > /dev/null
 
-echo "" >> ~/.bash_aliases # TODO
+echo "alias ls='exa -l --color=always --group-directories-first'
+alias la='exa -al --color=always --group-directories-first'
+" >> ~/.bash_aliases
+echo "Done installing Exa
+"
 
 # Neofetch install
 echo "Installing Neofetch..."
-sudo apt install neofetch -y
+sudo apt-get install neofetch -qq > /dev/null
 echo "
 neofetch
 " >> ~/.bashrc
+echo "Done installing Neofetch
+"
 
 # htop install
 echo "Installing Htop..."
-sudo apt install htop -y
+sudo apt-get install htop -qq > /dev/null
+echo "Done installing Htop
+"
 
-
-####################################### OS CUSTOMISATIONS #######################################
-
-# .bashrc customisation TODO
-
-
-# Message of the day TODO
-
-
-# Rename home falder to remove capitalized letters TODO
-
-
+# more packages? TODO
 
 ####################################### APPS #######################################
 
 # Brave install
-sudo apt install apt-transport-https curl -y
+echo "Installing Brave..."
+sudo apt-get install apt-transport-https curl -qq > /dev/null
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update -y
-sudo apt install brave-browser -y
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" \
+|sudo tee /etc/apt/sources.list.d/brave-browser-release.list > /dev/null
+sudo apt-get update -qq > /dev/null
+sudo apt-get install brave-browser -qq > /dev/null
+echo "Done installing Brave
+"
 
 # Discord install
 echo "Installing Discord..."
-sudo apt install discord -y
-
+sudo apt-get install discord -qq > /dev/null
+echo "Done installing Discord
+"
 
 ####################################### End of script #######################################
 
 # Cleanup
-sudo apt autoremove -y
-sudo apt clean -y
+sudo apt-get autoremove -qq > /dev/null
+sudo apt-get clean -qq > /dev/null
+
 
 # End message and notes #TODO
 echo "
 ______________________________________________________________________________________________________
 
                                         End of Deplux!
-
-                                            Notes
-
-In order to
-
 ______________________________________________________________________________________________________
 "
 
